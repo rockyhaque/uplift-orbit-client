28a3580c99b626ded58717cdda5c5ca3162c752a
Rocky Haque <rockyhaque99@gmail.com>
2024-07-11 22:07:21 +0600

    search functionality done

--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -8,6 +8,8 @@ const AllJobs = () => {
   const [count, setCount] = useState(0);
   const [filter, setFilter] = useState("");
   const [sort, setSort] = useState("");
+  const [search, setSearch] = useState('');
+  const [searchText, setSearchText] = useState('')
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
@@ -15,25 +17,25 @@ const AllJobs = () => {
       const { data } = await axios(
         `${
           import.meta.env.VITE_API_URL
-        }/allJobs?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}`
+        }/allJobs?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`
       );
       setJobs(data);
     };
     getData();
-  }, [currentPage, filter, itemsPerPage, sort]);
+  }, [currentPage, filter, itemsPerPage, sort, search]);
 
-  // console.log(jobs);
+ 
 
   useEffect(() => {
     const getCount = async () => {
       const { data } = await axios(
-        `${import.meta.env.VITE_API_URL}/jobsCount?filter=${filter}`
+        `${import.meta.env.VITE_API_URL}/jobsCount?filter=${filter}&search=${search}`
       );
 
       setCount(data.count);
     };
     getCount();
-  }, [filter]);
+  }, [filter, search]);
 
   const numberOfPages = Math.ceil(count / itemsPerPage);
   const pages = [...Array(numberOfPages).keys()].map((element) => element + 1);
@@ -47,7 +49,17 @@ const AllJobs = () => {
   const handleReset = () => {
     setFilter('');
     setSort('');
+    setSearch('');
+    setSearchText('')
   }
+
+  // search
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(searchText);
+  }
+
+  
 
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
@@ -73,12 +85,14 @@ const AllJobs = () => {
             </select>
           </div>
 
-          <form>
+          <form onSubmit={handleSearch}>
             <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
               <input
                 className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
                 type="text"
                 name="search"
+                onChange={(e)=>setSearchText(e.target.value)}
+                value={searchText}
                 placeholder="Enter Job Title"
                 aria-label="Enter Job Title"
               />
@@ -94,6 +108,7 @@ const AllJobs = () => {
               setSort(e.target.value);
               setCurrentPage(1);
             }}
+            value={sort}
               name="sort"
               id="sort"
               className="border p-4 rounded-md"
